e500697668b08b640e5948dd1938ce8886a92b7c
Kacper Piotrowski <s33174@pjwstk.edu.pl>
2026-04-08 22:45:09 +0200

    Add DiscountCalculator

---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/DiscountCalculator.cs
@@ -0,0 +1,23 @@
+﻿using LegacyRenewalApp.Discounts;
+
+namespace LegacyRenewalApp;
+
+public class DiscountCalculator
+{
+    private readonly IDiscountStrategy _strategy;
+
+    public DiscountCalculator(IDiscountStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public decimal CalculateDiscount(decimal baseAmount, decimal discountAmount)
+    {
+        return _strategy.CalculateDiscount(baseAmount, discountAmount);
+    }
+
+    public string DiscountNote()
+    {
+        return _strategy.DiscountNote();
+    }
+}
